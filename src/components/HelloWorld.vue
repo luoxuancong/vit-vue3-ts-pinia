7277a5042c5972d44b0e8cad461ef2b0028d2eb1
罗玄聪 <33108362+luoxuancong@users.noreply.github.com>
2022-05-07 11:30:19 +0800

    Add files via upload

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,14 @@
+ <template>
+       
+    <div>{{ userStore.name }}</div>
+     
+</template>
+
+ <script lang="ts" setup>
+import { useUserStore } from '@/store/user';
+
+const userStore = useUserStore();
+setTimeout(()=>{
+  userStore.name = 'lxc'
+},2000)
+</script>
---
+++ src/views/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="title">{{userStore.name}}</div>
+</template>
+
+ <script lang="ts" setup>
+import { useUserStore } from '@/store/user';
+
+const userStore = useUserStore();
+setTimeout(()=>{
+  userStore.name = 'lxc'
+},2000)
+</script>
+
+<style lang="less" scoped>
+  .title {
+    font-weight: bold;
+  }
+</style>
