ced1d61ec4790ef4ab5d8f4d04a11854bf3a4756
luoxuancong <742340151@qq.com>
2022-05-07 13:45:04 +0800

    chore: 配置文件

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,18 +1,23 @@
 <template>
-  <div class="title">{{userStore.name}}</div>
+    <div class="title">{{ userStore.name }}</div>
 </template>
 
  <script lang="ts" setup>
 import { useUserStore } from '@/store/user';
+import API from '@/api';
+
+const requestRes = async () => {
+    let result = await API.login('zhangsan', '123456');
+};
 
 const userStore = useUserStore();
-setTimeout(()=>{
-  userStore.name = 'lxc'
-},2000)
+setTimeout(() => {
+    userStore.name = 'lxc';
+}, 2000);
 </script>
 
 <style lang="less" scoped>
-  .title {
+.title {
     font-weight: bold;
-  }
+}
 </style>
